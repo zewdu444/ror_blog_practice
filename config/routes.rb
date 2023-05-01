fff87339268fb94f32b2773bda582ce29837585c
zewdu erkyhun <zewdu444@gmail.com>
2023-05-01 16:38:26 +0300

    Add Read article functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 end
