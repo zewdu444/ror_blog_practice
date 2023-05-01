1e04d0e9b988bb0e434082abddf5452dca41d726
zewdu erkyhun <zewdu444@gmail.com>
2023-05-01 17:46:01 +0300

    Add Edit functionalities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,17 @@ class ArticlesController < ApplicationController
     render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @article=Article.find(params[:id])
+  end
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+    redirect_to @article
+    else
+    render :edit, status: :unprocessable_entity
+    end
+  end
   private
   def article_params
     params.require(:article).permit(:title,:body)
